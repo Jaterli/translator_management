9a8d7e6fbf139cae6957d476913ddb829234c2e8
jt <jaterli@hotmail.com>
2024-12-11 13:44:57 +0100

    first commit

---
+++ translators/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+# from .models import Traductor, Combinacion
+
+# @admin.register(Traductor)
+# class TraductorAdmin(admin.ModelAdmin):
+#     list_display = ('nombre', 'apellidos', 'email', 'f_registro')
+#     search_fields = ('nombre', 'apellidos', 'email')
+
+# @admin.register(Combinacion)
+# class CombinacionAdmin(admin.ModelAdmin):
+#     list_display = ('idioma_origen', 'idioma_destino', 'traductor')
+#     search_fields = ('idioma_origen', 'idioma_destino')
