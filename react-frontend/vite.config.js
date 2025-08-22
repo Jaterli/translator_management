a7eadf98fe5a3571932cef49f3f36e401990ed34
Jaime TL <jaterli@hotmail.com>
2025-08-22 13:59:41 +0000

    update from droplet

---
+++ react-frontend/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        // proxy: {
+        //   '/api': 'http://localhost:8000', // Proxy hacia tu backend Django
+        // },
+        port: 5173,
+        strictPort: true,
+        open: true,
+    },
+    build: {
+        outDir: 'dist',
+    },
+});
