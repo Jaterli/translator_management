bcfa122d40c248b44cd0186a89a7f858c54cc571
jt <jaterli@hotmail.com>
2025-08-26 19:38:51 +0200

    chore: remove unnecessary files (round 1)

--- react-frontend/src/react-bootstrap.d.ts
+++
@@ -1,8 +0,0 @@
-// react-bootstrap.d.ts
-import 'react-bootstrap';
-
-declare module 'react-bootstrap' {
-  interface ButtonProps {
-    size?: 'sm' | 'lg' | string; // Permitir cadenas en `size`
-  }
-}
